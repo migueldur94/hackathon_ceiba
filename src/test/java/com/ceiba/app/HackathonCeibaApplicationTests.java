1688e676243294508028c443013dfd444dba5aad
Charles <nessjcharles@gmail.com>
2021-11-12 18:03:55 -0500

    se suben los fuentes iniciales

---
+++ src/test/java/com/ceiba/app/HackathonCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
