d4073c75b0adbe4f48bb0015c481395ed6d7a9e3
migueldur94 <migueldur94@gmail.com>
2021-11-13 12:25:11 -0500

    Se ajusto el host

--- src/main/java/com/ceiba/micro/controller/ServiceController.java
+++ src/main/java/com/ceiba/micro/controller/ServiceController.java
@@ -38,7 +38,7 @@ public class ServiceController {
         return new RestTemplate();
     }
 
-    @GetMapping("/healthz")
+    @GetMapping("/")
     @CircuitBreaker(name = HACKATHON_SERVICE, fallbackMethod = "hackathonFallback")
     @Bulkhead(name = HACKATHON_SERVICE, fallbackMethod = "hackathonFallback")
     @Retry(name = HACKATHON_SERVICE, fallbackMethod = "hackathonFallback")
